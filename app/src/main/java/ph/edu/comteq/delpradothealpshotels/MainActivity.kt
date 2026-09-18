4c8e70b4112d2982a5ae4d3c7e06ec579bce14c5
Neoh Del Prado <neohdp05@gmail.com>
2026-09-18 19:09:52 +0800

    feat: Homepage's general layout and coded hotels from json file
    - Add header
    - Extract and display hotel names from json

--- app/src/main/java/ph/edu/comteq/delpradothealpshotels/MainActivity.kt
+++ app/src/main/java/ph/edu/comteq/delpradothealpshotels/MainActivity.kt
@@ -1,16 +1,44 @@
 package ph.edu.comteq.delpradothealpshotels
 
+import android.R.attr.fontWeight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.gson.Gson
 import ph.edu.comteq.delpradothealpshotels.ui.theme.DelPradoTheAlpsHotelsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +48,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DelPradoTheAlpsHotelsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    Homepage(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +59,78 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Homepage(name: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var hotels by remember { mutableStateOf(emptyList<Hotel>()) }
+
+    // Load JSON data
+    LaunchedEffect(Unit) {
+        val json = context.assets.open("hotels.json")
+            .bufferedReader().use { it.readText() }
+        val gson = Gson()
+        val hotelsArray = gson.fromJson(json, Array<Hotel>::class.java)
+        hotels = hotelsArray.toList()
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        // --- HEADER ---
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(0xFF664930))
+                .height(80.dp) // Adjusted height to look more natural
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "The Alps Hotels",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFFFFDBBB)
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.france_national_flag),
+                contentDescription = "France Flag",
+                modifier = Modifier.size(35.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Image(
+                painter = painterResource(id = R.drawable.user_icon),
+                contentDescription = "User Icon",
+                modifier = Modifier.size(35.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = { search }
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(18.dp)
+        ) {
+            items(hotels) { hotel ->
+                Text(hotel.hotel_name)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomepagePreview() {
     DelPradoTheAlpsHotelsTheme {
-        Greeting("Android")
+        Homepage("Android")
     }
 }
