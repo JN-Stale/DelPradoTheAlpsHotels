513a433c38eb06e4361a6e7f3cb8c2045dea8277
Neoh Del Prado <neohdp05@gmail.com>
2026-09-25 14:36:00 +0800

    feat: implement hotel card layout and star rating display
    - Add HotelCard composable with coil AsyncImage for loading cover images from assets
    - Implement 5-star rating row using Icons star icon
    
    refactor: clean up hotel list rendering and fix compiler errors
    
    -Update HotelList to render HotelCard instead of basic text elements
    - Fix compiler errors, unresolved references, and removed unused imports and parameters.
    
    style: apply dark theme card styling and rounded layout borders.
    - apply dark theme background color to card container
    - add 20.dp rounded corner shape and white outline border to card layout
    - improve spacing and alignment for image, text, and rating details

--- app/src/main/java/ph/edu/comteq/delpradothealpshotels/MainActivity.kt
+++ app/src/main/java/ph/edu/comteq/delpradothealpshotels/MainActivity.kt
@@ -1,10 +1,12 @@
 package ph.edu.comteq.delpradothealpshotels
 
 import android.R.attr.fontWeight
+import android.icu.text.StringSearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -19,9 +21,17 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.StarRate
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -31,15 +41,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.HistoricalChange
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.google.gson.Gson
+import kotlin.math.roundToInt
 import ph.edu.comteq.delpradothealpshotels.ui.theme.DelPradoTheAlpsHotelsTheme
+import java.time.temporal.TemporalQuery
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +66,6 @@ class MainActivity : ComponentActivity() {
             DelPradoTheAlpsHotelsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Homepage(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -59,9 +75,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Homepage(name: String, modifier: Modifier = Modifier) {
+fun Homepage(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     var hotels by remember { mutableStateOf(emptyList<Hotel>()) }
+
+    // Add state to hold the current search text in the search bar
+    var searchQuery by remember { mutableStateOf(" ") }
 
     // Load JSON data
     LaunchedEffect(Unit) {
@@ -75,62 +94,184 @@ fun Homepage(name: String, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.fillMaxSize()
     ) {
-        // --- HEADER ---
+        // Header Composable
+        Header()
+
+        // SearchBar Composable
+        SearchBar(
+            searchQuery = searchQuery,
+            onSearchQueryChange = { searchQuery = it }
+        )
+
+        // Filter hotel list functionality based on what is searched
+        val filteredHotels = hotels.filter {
+            it.hotel_name.contains(searchQuery, ignoreCase = true)
+        }
+
+        // List Composable
+        HotelList(hotels = filteredHotels)
+    }
+}
+
+@Composable
+fun Header(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF664930))
+            .height(80.dp)
+            .padding(horizontal = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = "The Alphs Hotel",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFFFDBBB)
+        )
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.france_national_flag),
+            contentDescription = "France National Flag",
+            modifier = Modifier.size(35.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Icon(
+            imageVector = Icons.Outlined.Person,
+            contentDescription = "User Icon",
+            modifier = Modifier.size(35.dp)
+        )
+    }
+}
+
+@Composable
+fun SearchBar(
+    searchQuery: String,
+    onSearchQueryChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = searchQuery,
+        onValueChange = onSearchQueryChange,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        placeholder = { Text("Search hotels...") },
+        singleLine = true
+    )
+}
+
+@Composable
+fun HotelList(hotels: List<Hotel>){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        items(hotels) {
+            hotel -> HotelCard(hotel = hotel)
+        }
+    }
+}
+
+@Composable
+fun HotelCard(
+    hotel: Hotel,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF2C2C2C)
+        ),
+        border = BorderStroke(1.dp, Color.White.copy(alpha = 0.5f))
+    ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color(0xFF664930))
-                .height(80.dp) // Adjusted height to look more natural
-                .padding(horizontal = 16.dp),
+                .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = "The Alps Hotels",
-                style = MaterialTheme.typography.titleLarge,
-                fontWeight = FontWeight.Bold,
-                color = Color(0xFFFFDBBB)
-            )
-
-            Spacer(modifier = Modifier.width(12.dp))
-
-            Image(
-                painter = painterResource(id = R.drawable.france_national_flag),
-                contentDescription = "France Flag",
-                modifier = Modifier.size(35.dp),
+            AsyncImage(
+                model = "file:///android_asset/${hotel.hotel_cover_image}",
+                contentDescription = hotel.hotel_name,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(12.dp)),
                 contentScale = ContentScale.Crop
             )
 
-            Spacer(modifier = Modifier.weight(1f))
+            Spacer(modifier = Modifier.width(16.dp))
 
-            Image(
-                painter = painterResource(id = R.drawable.user_icon),
-                contentDescription = "User Icon",
-                modifier = Modifier.size(35.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
+            // Information Column
+            Column(
+                modifier = Modifier.weight(1f),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                // Hotel Name
+                Text(
+                    text = hotel.hotel_name,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
 
-        OutlinedTextField(
-            value = searchQuery,
-            onValueChange = { search }
-        )
+                // Stars Row using star from Icon
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(2.dp)
+                ) {
+                    Text(
+                        text = "Rating: ",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = Color.White
+                    )
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(18.dp)
-        ) {
-            items(hotels) { hotel ->
-                Text(hotel.hotel_name)
+                    val stars = (hotel.hotel_rating / 2.0).roundToInt().coerceIn(0, 5)
+                    repeat(5) { index ->
+                        Icon(
+                            imageVector = Icons.Filled.StarRate,
+                            contentDescription = "Star Rating",
+                            tint = if (index < stars) {
+                                Color(0xFFFFC107)
+                            }else{
+                                Color.Gray
+                            },
+                            modifier = Modifier.size(18.dp)
+                        )
+                    }
+                }
+
+                // Description
+                Text(
+                    text = "Description",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.LightGray
+                )
+
+                // Distance
+                Text(
+                    text = "${hotel.hotel_to_ski_distance} km to ski slope",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.LightGray
+                )
             }
         }
     }
 }
 
+
 @Preview(showBackground = true)
 @Composable
 fun HomepagePreview() {
     DelPradoTheAlpsHotelsTheme {
-        Homepage("Android")
+        Homepage()
     }
 }
